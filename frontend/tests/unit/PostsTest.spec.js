5fde932748321b1cf7a0881ab18fa7d1a1d0433c
PoolaAndreas <poolaandreas@gmail.com>
2020-12-12 01:10:03 +0200

    Testing done

--- frontend/tests/unit/PostsTest.spec.js
+++ frontend/tests/unit/PostsTest.spec.js
@@ -98,37 +98,58 @@ jest.mock("axios", () => ({
 
 describe('Posts', () => {
     const wrapper = mount(Posts, {router, store, localVue});
+
     it('NumberPosts',() => {
-        expect(testData.length==3).toBe(true)
+        const divpost = wrapper.findAll('[class="post"]')
+        expect(divpost).toHaveLength(testData.length)
     })
     it('media',() =>{
-        let hasMedia = 0
-        for(var i = 0; i<testData.length;i++){
-            if(testData[i].hasOwnProperty('media')==true){
-                if(testData[i]['media'] == null){
-                    hasMedia++
-
-                }else {
-                    if (testData[i]['media'].hasOwnProperty('type')==true && testData[i]['media'].hasOwnProperty('url')==true   ) {
-                        hasMedia++
-
-                    }
-                }
-            }
-
-        }
-        expect(hasMedia==testData.length).toBe(true)
+        const divpost = wrapper.findAll('[class="post-image"]')
+        const divpostimg = wrapper.findAll('div div div div img')
+        const divpostvid = wrapper.findAll('video')
+        expect(divpost.exists()).toBe(true)
+        //if there is no media at all this returns false
+        expect(divpostimg).toHaveLength(1)
+        expect(divpostvid).toHaveLength(1)
+        //because there is only 1 video rendered
     })
 
     it('1 == 1', function () {
         expect(true).toBe(true)
     });
     it('dateformat', () =>{
-        let correctDate = 0
-        for(var i = 0;i<testData.length;i++){
-            console.log(testData[i]['createTime'])
-            //if(testData[i]['createTime'].DateTimeFormat())
+        const divpost = wrapper.findAll('[class="date"]')
+        var options = {weekday:'long'}
+        var options2 = {month: 'long'}
+        for(var i = 0; i<  divpost.length;i++){
+            let datesplit = divpost.at(i).text().split(',')
+            let day = (datesplit[0])
+            if(day ==="Sunday"||day==="Monday"||day==="Tuesday"||day ==="Wednesday"||day==="Thursday"||day==="Friday"||day==="Saturday"){
+                expect(true).toBe(true)
+            }
+            let month = (datesplit[1].split(" "))[1]
+            if(month==="January"||month==="February"||month==="March"||month==="April"||month==="May"||month==="June"||month==="July"||month==="August"||month==="September"||month==="October"||month==="November"||month==="December"){
+                expect(true).toBe(true)
+            }
+            let day2 = (datesplit[1].split(" "))[2]
+            if(day2<32 && day2>0){
+                expect(true).toBe(true)
+            }
+            let year = (datesplit[2].split(" "))[1]
+            if(year<2030 && year>1900){
+                expect(true).toBe(true)
+
+            }
+            let clock1 = (datesplit[2].split(" "))[2].split(":")
+            if(clock1[0]<=12 && clock1[0]>=0 && clock1[1]>=0 && clock1[1]<=60){
+                expect(true).toBe(true)
+
+            }
+            let clock2 = (datesplit[2].split(" "))[3]
+            if(clock2==="AM"||clock2==="PM"){
+                expect(true).toBe(true)
+
+            }
         }
-        expect()
     })
 });
